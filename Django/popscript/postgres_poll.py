69a4d6fd609aa3d9fa1cdd40dec758adc918c99a
Dan Weiss <dweiss@nuodb.com>
2013-08-09 14:05:54 -0400

    fixed

--- Django/popscript/postgres_poll.py
+++ Django/popscript/postgres_poll.py
@@ -4,8 +4,8 @@ con = psycopg2.connect("dbname=this user=postgres")
 
 cursor = con.cursor()
 for i in xrange(0, 1000):
-    cursor.execute("insert into polls_poll values (%d, %s, %s)", 
-                   (i, str(i) + "?", "now"))
+    cursor.execute("insert into polls_poll values (%s, %s, %s)", 
+                   (str(i), str(i) + "?", "now"))
 con.commit()
 cursor.close()
 con.close() 
