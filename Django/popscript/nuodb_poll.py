9114a2e038b3f4eda755bfadf54279812d84ce82
Dan Weiss <dweiss@nuodb.com>
2013-08-23 11:56:26 -0400

    updated to use executemany

--- Django/popscript/nuodb_poll.py
+++ Django/popscript/nuodb_poll.py
@@ -1,9 +1,13 @@
 import pynuodb
 
 con = pynuodb.connect("Thousand", "localhost", "dba", "goalie", options = {"schema":"user"})
+con.auto_commit = True
 
 cursor = con.cursor()
-for i in xrange(0, 1000):
-    test_vals = (i, str(i) + "?", "now")
-    cursor.execute("insert into user.polls_poll values (?, ?, ?)", test_vals)
-con.commit() 
+# Insert 10 million rows
+for i in range(1, 5001):
+    test_batch = []
+    for j in xrange(0, 2000):
+        test_batch.append([i * j, str(i * j) + "?", "now"])
+        cursor.executemany("insert into user.polls_poll values (?, ?, ?)", test_batch)
+
