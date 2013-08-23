4b455ee795b241865b6ead45f2e5a91d43a0e604
Dan Weiss <dweiss@nuodb.com>
2013-08-23 13:06:08 -0400

    fixed id numbers

--- Django/popscript/nuodb_poll.py
+++ Django/popscript/nuodb_poll.py
@@ -5,9 +5,10 @@ con.auto_commit = True
 
 cursor = con.cursor()
 # Insert 10 million rows
-for i in range(1, 5001):
+for i in xrange(0, 5000):
     test_batch = []
     for j in xrange(0, 2000):
-        test_batch.append([i * j, str(i * j) + "?", "now"])
+        id = (i * 2000) + j
+        test_batch.append([id, str(id) + "?", "now"])
         cursor.executemany("insert into user.polls_poll values (?, ?, ?)", test_batch)
 
