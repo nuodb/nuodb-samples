9e61c01ef5a437d18e91d31ca4e4921c19360456
Tim Vasil <tvasil@nuodb.com>
2015-07-21 22:01:52 -0400

    [DB-11391] Fix: Don't allow chart toggle buttons to be depressed

--- StorefrontDemo/src/main/webapp/admin/app-js/view/ChartControlBar.js
+++ StorefrontDemo/src/main/webapp/admin/app-js/view/ChartControlBar.js
@@ -17,37 +17,44 @@ Ext.define('App.view.ChartControlBar', {
             text: '1 min',
             handler: me.onClickHandler,
             toggleGroup: 'x',
-            pressed: true
+            pressed: true,
+            allowDepress: false
         }, {
             text: '2 min',
             handler: me.onClickHandler,
-            toggleGroup: 'x'
+            toggleGroup: 'x',
+            allowDepress: false
         }, {
             text: '5 min',
             handler: me.onClickHandler,
-            toggleGroup: 'x'
+            toggleGroup: 'x',
+            allowDepress: false
         }, {
             text: '10 min',
             handler: me.onClickHandler,
-            toggleGroup: 'x'
+            toggleGroup: 'x',
+            allowDepress: false
         }, {
             text: '15 min',
             handler: me.onClickHandler,
-            toggleGroup: 'x'
+            toggleGroup: 'x',
+            allowDepress: false
         }, ' ', '-', ' ', '<b>Y AXIS:</b>', ' ', {
             text: 'Auto-adjust',
             handler: me.onClickHandler,
             toggleGroup: 'y',
             pressed: !App.app.lockStatsYAxisToMax,
             tooltip: 'Adjusts the maximum value of the y axis to the largest value currently visible.',
-            tooltipType: 'title'
+            tooltipType: 'title',
+            allowDepress: false
         }, {
             text: 'Lock to peak',
             handler: me.onClickHandler,
             toggleGroup: 'y',
             pressed: App.app.lockStatsYAxisToMax,
             tooltip: 'Fixes the maximum value of the y axis to the largest value encountered, even if that value is no longer visible.',
-            tooltipType: 'title'
+            tooltipType: 'title',
+            allowDepress: false
         }, '->'];
 
         me.callParent(arguments);
