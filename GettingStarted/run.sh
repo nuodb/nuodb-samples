f614fe332e40ce7c0ebc18b0ce0b2a148d21e358
NikTJ777 <ntrevallyn@nuodb.com>
2017-01-30 15:53:43 -0500

    * add -time command argument to run.sh script

--- GettingStarted/run.sh
+++ GettingStarted/run.sh
@@ -3,5 +3,11 @@
 
 export NUODB_HOME=${NUODB_HOME:-/opt/nuodb}
 
-java -cp .:$NUODB_HOME/jar/nuodbjdbc.jar GettingStarted -url jdbc:com.nuodb://localhost/testdb -user dba -password dba -threads 10
+if [ $1 = "-time" ] && [ "x$2" != "x" ]; then
+	export runTime=$2
+else
+	export runTime=1
+fi
+
+java -cp .:$NUODB_HOME/jar/nuodbjdbc.jar GettingStarted -url jdbc:com.nuodb://localhost/testdb -user dba -password dba -threads 10 -time $runTime
 
